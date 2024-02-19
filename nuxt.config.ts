ec09f7d048c7077488bd976559727fa7f812e5d0
Iwakura-dev <escayhokage@gmail.com>
2024-02-19 06:42:55 -0800

    style: 💄 tailwind
    add tailwindcss

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,5 +1,11 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
   devtools: { enabled: true },
-  modules: ['@nuxtjs/eslint-module']
+  modules: ['@nuxtjs/eslint-module'],
+  postcss: {
+    plugins: {
+      tailwindcss: {},
+      autoprefixer: {}
+    }
+  }
 })
